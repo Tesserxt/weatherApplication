d3adbbaf0c6a2dfd605783f577ed93f19eac8e9e
Tesserxt <142136334+Tesserxt@users.noreply.github.com>
2024-12-02 10:49:50 +0530

    Created objects out of JSON data
    -date, temprature, humidity, windSpeed and weatherCondition are stored into respective variables.

--- src/main/java/weatherPkg/weatherServlet.java
+++ src/main/java/weatherPkg/weatherServlet.java
@@ -6,7 +6,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.net.URI;
+import java.sql.Date;
+import java.util.Scanner;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.GsonBuilder;
+import javax.net.ssl.HttpsURLConnection;
 /**
  * Servlet implementation class weatherServlet
  */
@@ -34,9 +41,45 @@ public class weatherServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String city = request.getParameter("city"); 
-		System.out.println(city);
+		//API_Setup
+		String city = request.getParameter("city");
+		String apiKey = "6f18b4020229383ecf0c6f8acbd4adbf";
+		String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city + "&appid=" + apiKey;
+		
+		StringBuilder responseContent =  new StringBuilder();
+		try {
+		    var url = URI.create(apiUrl).toURL();
+		    HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+		    connection.setRequestMethod("GET");
+		    
+		    try (InputStream inputStream = connection.getInputStream();
+		         Scanner scanner = new Scanner(inputStream)) {
+		        while (scanner.hasNext()) {
+		            responseContent.append(scanner.nextLine());
+		        }
+		    }
+		} catch (IOException e) {
+		    System.err.println("An error occurred: " + e.getMessage());
+		}
+
+        System.out.println(responseContent);		
+		Gson gson = new Gson();
+		JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
+
+		long dateTimeStamp = jsonObject.get("dt").getAsLong() * 1000;
+		String date = new Date(dateTimeStamp).toString();
+		
+		double tempratureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+		int tempratureCelsius =  (int) (tempratureKelvin - 273.15);
+		
+		int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+		
+		double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+		
+		String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+		
+		System.out.println(weatherCondition);
+		System.out.println("City " + city);
 		doGet(request, response);
 	}
 
